75a463f3e8d2c2874f7c484751fee1df303e0381
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2024-02-04 21:41:18 +0700

    add midleware folder

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const config = require("../config/config");
-const jwt = require('jsonwebtoken');
-
-// router.post("/login", (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     res.json({ username, password });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("../middleware/admin");
 
 // Secret key untuk JWT, sebaiknya simpan di environment variable
 const secretKey = config.secretKey;
@@ -25,7 +17,7 @@ router.post("/login", async (req, res) => {
   // Gantilah ini dengan validasi pengguna dari database
   //   const user = await User.findOne({ username });
 
-  if (User.password != password) {
+  if (User.password != password && User.username != username) {
     return res.status(401).json({ message: "Autentikasi gagal" });
   }
 
@@ -37,20 +29,7 @@ router.post("/login", async (req, res) => {
 
 // Rute terproteksi yang membutuhkan token untuk akses
 router.get("/dashboard", authenticateToken, (req, res) => {
-  res.json({ message: "Halaman profil terproteksi" });
+  res.json({ message: "Halaman Admin terproteksi" });
 });
-
-// Middleware untuk memverifikasi token
-function authenticateToken(req, res, next) {
-  const token = req.header("Authorization");
-
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token, secretKey, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
 
 module.exports = router;
