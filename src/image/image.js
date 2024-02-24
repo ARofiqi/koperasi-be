e3e7a3f26b0a7edc83ddefef198fc34ccfa36d10
prajadiputra22 <darmawan.prajadiputra_ti22@nusaputra.ac.id>
2024-02-24 21:57:50 +0700

    commit

--- src/image/image.js
+++ src/image/image.js
@@ -1,46 +1,22 @@
 const sharp = require('sharp');
 const fs = require('fs');
 
-const inputImagePath = 'src/image/download.jpeg';
+// Path file input dan output
+const inputImagePath = 'src/image/IMG_20231010_132321.jpg';
 const outputImagePath = 'src/image/image.webp';
 
-function convertAndCropToWebP(inputPath, outputPath, resize) {
-  if (fs.existsSync(inputPath)) {
-    // Baca gambar dari file
-    const image = sharp(inputPath);
-
-    // Dapatkan informasi ukuran gambar
-    image.metadata((err, metadata) => {
+// Fungsi untuk mengonversi gambar ke format WebP
+function convertToWebP(inputPath, outputPath) {
+  sharp(inputPath)
+    .webp() // Menggunakan format WebP
+    .toFile(outputPath, (err, info) => {
       if (err) {
         console.error(err);
-        return;
+      } else {
+        console.log('Gambar berhasil dikonversi ke WebP:', info);
       }
-
-      // Hitung koordinat agar crop berada di tengah
-      const centerX = Math.floor((metadata.width - resize.width) / 2);
-      const centerY = Math.floor((metadata.height - resize.height) / 2);
-
-      // Lakukan cropping dengan koordinat yang dihitung
-      image.extract({ left: centerX, top: centerY, width: resize.width, height: resize.height });
-
-      // Konversi ke format WebP
-      image.webp().toFile(outputPath, (err, info) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Gambar berhasil dikonversi ke WebP:', info);
-        }
-      });
     });
-  } else {
-    console.error('File input tidak ditemukan.');
-  }
 }
 
-// Contoh penggunaan: Crop gambar dari koordinat (left, top) ke (right, bottom)
-const resize = {
-  width: 150,
-  height: 120,
-};
-
-convertAndCropToWebP(inputImagePath, outputImagePath, resize);
+// Panggil fungsi convertToWebP dengan path gambar input dan output
+convertToWebP(inputImagePath, outputImagePath);
